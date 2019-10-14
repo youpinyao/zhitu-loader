dec74eb22ae892908881e968b5922b5b4333ff49
游品尧 <497400448@qq.com>
2019-10-14 11:03:33 +0800

    1

--- postinstall.js
+++ postinstall.js
@@ -1,8 +1,10 @@
 /* eslint-disable */
 
 var extract = require('extract-zip')
+var fse = require('fs-extra');
 var path = require('path');
 extract(path.resolve(__dirname, 'app.zip'), { dir: path.resolve(__dirname, 'app') }, function (err) {
   // extraction is complete. make sure to handle the err
   err && console.log(err);
+  fse.removeSync(path.resolve(__dirname, 'app.zip'));
 })
